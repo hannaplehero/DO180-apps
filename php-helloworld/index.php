0660bdee9a90b5561ded16788d1c5633346eb9d5
Student User <student@workstation.lab.example.com>
2021-09-05 17:05:42 -0400

    hogehoge

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A Change is int the air!\n";
-print "hogehbnoge";
-print "hogehoge";
+print "hogehbnoge\n";
+print "hogehoge\n";
 ?>
